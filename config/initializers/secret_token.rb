19fcab537c46e009b057c9e530769490bd6a9d8f
Greg Sterndale <gsterndale@gmail.com>
2011-11-22 09:20:02 -0500

    Base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clinical::Application.config.secret_token = '3ad797eb1ce8dbd47e348f8df55e25ba8e3bb61b965dc064de75d2e42fc51d196aa299059f33c53879ae7898a23f32f9663a322b06322881c7788a8b9d5d61d8'
